d92c0de15a4982c59476c0581e5403dded0c51c2
ayoubmn <ayoubmn.1999n1@gmail.com>
2020-12-29 01:48:24 +0100

    adding User entity

--- src/main/java/com/engineerstalk/ws/ui/controller/UserController.java
+++ src/main/java/com/engineerstalk/ws/ui/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.engineerstalk.ws.ui.controller;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.engineerstalk.ws.service.UserService;
 import com.engineerstalk.ws.shared.dto.UserDto;
 import com.engineerstalk.ws.ui.model.request.UserDetailsRequestModel;
 import com.engineerstalk.ws.ui.model.response.UserRest;
@@ -17,6 +19,9 @@ import com.engineerstalk.ws.ui.model.response.UserRest;
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
 	@GetMapping
 	public String getUser() {
 		return "get user was called";
